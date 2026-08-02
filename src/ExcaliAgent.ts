3ddc2bd3c1035e0110d3ca5838f4a03c204dce18
arham <imranarham798@gmail.com>
2026-08-02 14:04:53 +0500

    test cloudflare AIChatAgent with AI vercel sdk

--- src/ExcaliAgent.ts
+++ src/ExcaliAgent.ts
@@ -1,44 +1,22 @@
-import { Agent, WSMessage, type Connection } from 'agents';
-import { generateText, streamText } from 'ai';
+import { AIChatAgent } from '@cloudflare/ai-chat';
+import { streamText, convertToModelMessages, createUIMessageStream, createUIMessageStreamResponse, toUIMessageStream } from 'ai';
 import { createWorkersAI } from 'workers-ai-provider';
 import { tools } from './tools';
 
-type Message = {
-	role: 'user' | 'assistant';
-	content: string;
-};
+export class ExcaliAgent extends AIChatAgent<Env> {
+	async onChatMessage() {
+		console.log('received message');
+		const workersai = createWorkersAI({ binding: this.env.AI });
 
-export class ExcaliAgent extends Agent<Env> {
-	async onConnect(connection: Connection) {
-		connection.send('WebSocket connection established');
-	}
+		const result = streamText({
+			model: workersai('@cf/ibm-granite/granite-4.0-h-micro'),
+			instructions: 'You are a weather agent that provides weather information to the user. you can check the weather',
+			messages: await convertToModelMessages(this.messages),
+			tools,
+		});
 
-	async onMessage(connection: Connection, message: WSMessage) {
-		console.log('got:', message.toString());
-
-		const history = (await this.ctx.storage.get<Message[]>('history')) ?? [];
-		history.push({ role: 'user', content: message.toString() });
-
-		const provider = createWorkersAI({ binding: this.env.AI });
-		const model = provider('@cf/ibm-granite/granite-4.0-h-micro');
-
-		const result = streamText({ model, messages: history, tools });
-
-		let aiResponse = '';
-		for await (const chunk of result.textStream) {
-			connection.send(chunk);
-			aiResponse += chunk;
-		}
-
-		history.push({ role: 'assistant', content: aiResponse });
-		await this.ctx.storage.put('history', history);
-	}
-
-	async onClose(connection: Connection, code: number, reason: string) {
-		console.log('closed:', code, reason);
-	}
-
-	async onError(connection: unknown, error?: unknown): Promise<void> {
-		console.log('error', error ?? 'no info');
+		return createUIMessageStreamResponse({
+			stream: toUIMessageStream({ stream: result.stream }),
+		});
 	}
 }
