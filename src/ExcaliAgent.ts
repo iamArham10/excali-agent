472ffe2fce509187c94dfddcc1334f4e08a133c6
arham <imranarham798@gmail.com>
2026-08-02 11:44:21 +0500

    checked webSocket

--- src/ExcaliAgent.ts
+++ src/ExcaliAgent.ts
@@ -9,36 +9,54 @@ type Message = {
 
 export class ExcaliAgent extends Agent<Env> {
 	async fetch(request: Request): Promise<Response> {
-		const url = new URL(request.url);
-		const query = url.searchParams.get('query') ?? '';
-
-		// load history
-		const history = (await this.ctx.storage.get<Message[]>('history')) ?? [];
-
-		// add user message to history
-		history.push({ role: 'user', content: query });
-		const provider = createWorkersAI({
-			binding: this.env.AI,
-		});
-
-		// generate response
-		const model = provider('@cf/meta/llama-3.2-3b-instruct');
-		const result = streamText({ model: model, messages: history });
-
-		let answer = '';
-		for await (const chunk of result.textStream) {
-			answer += chunk;
-			console.log(chunk);
+		// check for WebSocket upgrade
+		const upgrade = request.headers.get('Upgrade');
+		if (upgrade !== 'websocket') {
+			return new Response('Expected WebSocket upgrade', { status: 426 });
 		}
 
-		// add assistant message to history
-		history.push({ role: 'assistant', content: answer });
+		// create a websocket pair
+		const [client, server] = Object.values(new WebSocketPair());
+		server.accept();
 
-		// save history
-		await this.ctx.storage.put('history', history);
+		// listen for messages from the client
+		server.addEventListener('message', async (event) => {
+			const userMessage = event.data.toString();
 
-		return Response.json({
-			reply: result,
+			// load history
+			const history = (await this.ctx.storage.get<Message[]>('history')) ?? [];
+
+			// add user message to history
+			history.push({ role: 'user', content: userMessage });
+			const provider = createWorkersAI({
+				binding: this.env.AI,
+			});
+
+			const model = provider('@cf/meta/llama-3.2-3b-instruct');
+			const result = streamText({ model: model, messages: history });
+
+			let answer = '';
+			for await (const chunk of result.textStream) {
+				answer += chunk;
+				server.send(chunk);
+			}
+
+			history.push({
+				role: 'assistant',
+				content: answer,
+			});
+
+			await this.ctx.storage.put('history', history);
+		});
+
+		// close the server
+		server.addEventListener('close', () => {
+			client.close();
+		});
+
+		return new Response(null, {
+			status: 101,
+			webSocket: client,
 		});
 	}
 }
